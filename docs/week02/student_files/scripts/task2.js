80c8ae79257d9f8e88127e83b708380ad8e6bd0a
Tim Thayne <timthayne@gmail.com>
2020-02-26 16:07:27 -0700

    Added Task 3 content

---
+++ docs/week02/student_files/scripts/task2.js
@@ -0,0 +1,56 @@
+/* Lesson 2 */
+
+/* VARIABLES */
+
+// Step 1: declare and instantiate a variable to hold your name
+
+
+// Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
+
+
+// Step 3: declare and instantiate a variable to hold the current year
+
+
+// Step 4: place the value of the current year variable into the HTML file
+
+
+// Step 5: declare and instantiate a variable to hold the name of your picture
+
+
+// Step 6: copy your image into the "images" folder
+
+// Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
+
+
+
+
+/* ARRAYS */
+
+// Step 1: declare and instantiate an array variable to hold your favorite foods
+
+
+// Step 2: place the values of the favorite foods variable into the HTML file
+
+
+// Step 3: declare and instantiate a variable to hold another favorite food
+
+
+// Step 4: add the variable holding another favorite food to the favorite food array
+
+
+// Step 5: repeat Step 2
+
+
+// Step 6: remove the first element in the favorite foods array
+
+
+// Step 7: repeat Step 2
+
+
+// Step 8: remove the last element in the favorite foods array
+
+
+// Step 7: repeat Step 2
+
+
+
