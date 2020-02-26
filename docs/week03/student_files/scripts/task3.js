d05db923f114fd70314bb978207814de56368170
Tim Thayne <timthayne@gmail.com>
2020-02-26 16:12:32 -0700

    Added task3.js to Student Files folder/Rezipped

---
+++ docs/week03/student_files/scripts/task3.js
@@ -0,0 +1,32 @@
+/* Lesson 3 */
+
+/* FUNCTIONS */
+
+// Step 1: Declare a function named sum that takes two arguments, number1 and number2
+
+// Step 2: In the function, return the sum of the parameters number1 and number2
+
+// Step 3: Declare another function named sumNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the sum function
+
+// Step 4: Assign the return value to an HTML form element with an id of sum
+
+// Step 5: Note in the task3.html HTML file there is a reference to the sumNumbers function in the onclick attribute of the first button.
+
+// Step 6: Repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend and difference
+
+// Step 7: Repeat Steps 1-5 with new functions named multiply and mulitplyNumbers and HTML form controls with IDs of factor1, factor2 and product
+
+// Step 8: Repeat Steps 1-5 with new functions named divide and divideNumbers and HTML form controls with IDs of dividend, divisor and quotient
+
+// Step 9: Test all of the mathematical functionality of the task3.html page.
+
+
+/* BUILT-IN METHODS */
+
+// Step 1: Declare and instantiate a variable of type Date to hold the current date
+
+// Step 2: Declare a variable to hold the current year
+
+// Step 3: Using the variable declared in Step 1, call the built-in getFullYear() method/function and assign it to the variable declared in Step 2
+
+// Step 4: Assign the current year variable to an HTML form element with an id of year
