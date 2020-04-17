0117464cca397b1ad156a40d8bde8167b1dca7cb
yenrabbyui <yenrabbyui@gmail.com>
2020-04-17 05:02:35 -0600

    rationalized the placing of the proposed solution files

--- docs/week03/scripts/main.js
+++
@@ -1,7 +0,0 @@
-// toggle menu in small view
-const toggleMenu = () => {
-    document.querySelector('#menu').classList.toggle('open');
-}
-
-document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
-
